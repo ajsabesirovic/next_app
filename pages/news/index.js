78823e07b2d9f91318455f759f58c0773c11764b
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-07-16 18:15:34 +0200

    linking beetween pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,4 +1,20 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 function NewsPage() {
-  return <h1>The news page</h1>;
+  return (
+    <Fragment>
+      <h1>The news page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextJS-is-great">NextJS is a great framework</Link>
+        </li>
+
+        <li>
+          <Link href="/news/react-best">React is the best </Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 }
 export default NewsPage;
