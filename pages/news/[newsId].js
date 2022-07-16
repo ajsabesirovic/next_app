6e6689eed653418737fed4d46633d21cb1601a12
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-07-16 17:50:26 +0200

    extracting dynamic parameter vallues

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,8 @@
+import { useRouter } from "next/router";
+
 function DetailPage() {
+  const router = useRouter();
+  const newsId = router.query.newsId;
   return <h1>The detail page</h1>;
 }
 export default DetailPage;
