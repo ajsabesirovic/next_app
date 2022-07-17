ce92846ba028f5da423442e69fad80f74d7f70ab
Ajsa Besirovic <besirovicajsa@gmail.com>
2022-07-17 11:45:09 +0200

    outputing a list of meetups

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,23 @@
+import MeetupList from "../components/meetups/MeetupList";
+const dummy_meetups = [
+  {
+    id: "m1",
+    title: "The first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    adress: "Some adress 5, 12345 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "The second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+    adress: "Some adress 10, 12345 Some City",
+    description: "This is a second meetup",
+  },
+];
 function HomePage() {
-  return <h1>The home page</h1>;
+  return <MeetupList meetups={dummy_meetups} />;
 }
 export default HomePage;
